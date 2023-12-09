7807fe909d3ad82d605f91c346413ec142ba9607
rdelian <darkyzzx@gmail.com>
2023-12-09 03:17:08 +0200

    ⚡: init skins

---
+++ skins/wmr5.lua
@@ -0,0 +1,224 @@
+local fileName = debug.getinfo(1,'S').source:match("^.+/(.+)$"):gsub('.lua', '')
+local skinData = {
+	-- names
+	skinName = fileName,
+	ytdName = fileName,
+	-- texture dictionary informations:
+	-- night textures are supposed to look like this:
+	-- "needle", "tachometer", cst.ytdName, "fuelgauge"
+	-- daytime textures this:
+	-- "needle_day", "tachometer_day", "speedometer_day", "fuelgauge_day"
+	-- these names are hardcoded
+
+	-- where the speedo gets centered, values below are OFFSETS from this.
+	centerCoords = {0.8,0.8},
+
+
+	-- icon locations
+	lightsLoc = {0.015,0.15,0.018,0.02},
+	blinkerLoc = {0.04,0.15,0.022,0.03},
+	fuelLoc = {-0.005,0.15,0.012,0.025},
+	oilLoc = {0.100,0.15,0.020,0.025},
+	engineLoc = {0.130,0.15,0.020,0.025},
+
+	-- gauge locations
+	--[[UNUSED]]SpeedoBGLoc = {0.0625, 0.019, 0.238,0.238},
+	--[[UNUSED]]SpeedoNeedleLoc = {0.0,5,0.076,0.15},
+
+	TachoBGloc = {0.110,0.02,0.18,0.3},
+	TachoNeedleLoc = {0.110,0.020, 0.09, 0.16},
+	RevLight = {0.110, 0.02, 0.18,0.3},
+
+	TurboBGLoc = {0.053, -0.150, 0.18 / 2.6, 0.3 / 2.6},
+	TurboGaugeLoc = {0.053, -0.150, 0.09 / 2, 0.16 / 2},
+
+	--[[UNUSED]]FuelBGLoc = {0.110, 0.093,0.050, 0.040},
+	--[[UNUSED]]FuelGaugeLoc = {0.060,0.000,0.030,0.080},
+	enableDigits = true,
+	enableGear = true,
+
+	-- you can also add your own values and use them in the code below, the sky is the limit!
+	GearLoc = {0.13,-0.17, 0.1, 0.16}, -- gear location
+	Speed1Loc = {-0.038,0.042,0.030,0.055}, -- 3rd digit
+	Speed2Loc = {-0.010,0.042,0.030,0.055}, -- 2nd digit
+	Speed3Loc = {0.018,0.042,0.030,0.055}, -- 1st digit
+	UnitLoc = {0.018,0.085,0.033,0.023},
+
+	RotMult = 2.036936,
+	RotStep = 2.32833,
+
+	-- rpm scale, defines how "far" the rpm gauge goes before hitting redline
+	rpmScaleDecrease = 130,
+	rpmScale = 300,
+
+}
+
+addSkin(skinData)
+
+
+-- addon code
+
+local idcars = {"FUTO", "AE86", "86", "BLISTA2"} -- cars that use the AE86 speed chime and ae86 RPM background
+local labelType = "8k"
+local curDriftAlpha = 0
+local useKPH = GetResourceKvpString("initiald_unit") -- handle our unit saving
+if not useKPH then
+	SetResourceKvp("initiald_unit", "true")
+	useKPH = true
+end
+if useKPH	== "true" then
+	useKPH = true
+elseif useKPH == "false" then
+	useKPH = false
+end
+
+local function angle(veh)
+	if not veh then return false end
+	local vx,vy,vz = table.unpack(GetEntityVelocity(veh))
+	local modV = math.sqrt(vx*vx + vy*vy)
+
+
+	local rx,ry,rz = table.unpack(GetEntityRotation(veh,0))
+	local sn,cs = -math.sin(math.rad(rz)), math.cos(math.rad(rz))
+
+	if speed* 3.6 < 40 or gear == 0 then return 0,modV end --speed over 25 km/h
+
+	local cosX = (sn*vx + cs*vy)/modV
+	return math.deg(math.acos(cosX))*0.5, modV
+end
+local function BlinkDriftText(hide)
+	if overwriteAlpha then curDriftAlpha = 0 return end
+	if hide == true or goDown == true then
+		curDriftAlpha = curDriftAlpha-15
+	elseif not hide or goDown == false then
+		curDriftAlpha = curDriftAlpha+15
+	end
+	if curDriftAlpha <= 0 then
+		curDriftAlpha = 0
+		goDown = false
+	elseif curDriftAlpha >= 255 then
+		curDriftAlpha = 255
+		if driftSprite ~= "drift_yellow" then
+			goDown = true
+		end
+	end
+end
+SpeedChimeActive = false
+
+
+Citizen.CreateThread(function()
+	repeat
+		Wait(50)
+	until scriptReady
+	while true do
+		Citizen.Wait(0)
+		if getCurrentSkin() == skinData.skinName and (inVehicleAtGetin or inVehicle) then
+
+			toggleFuelGauge(false)
+			if DoesCurrentVehExist then
+				if vehclass >= 0 and vehclass <= 5 then
+					labelType = "8k"
+					cst.rpmScale = 235
+				elseif vehclass == 6 then
+					labelType = "9k"
+					cst.rpmScale = 235
+				elseif vehclass == 7 then
+					labelType = "10k"
+					cst.rpmScale = 235
+				elseif vehclass == 8 then
+					labelType = "13k"
+					cst.rpmScale = 235
+				end
+				for i,theName in ipairs(idcars) do
+					if string.find(vehdisplayname, theName) ~= nil and string.find(vehdisplayname, theName) >= 0 then
+						labelType = "86"
+						cst.rpmScale = 242
+					end
+					if vehdisplayname == theName then
+						if not SpeedChimeActive and speed*3.6 > 105.0 then
+							SpeedChimeActive = true
+							TriggerEvent("initiald:Sound:PlayOnOne","initiald",0.7,true)
+						elseif SpeedChimeActive and speed*3.6 < 105.0 then
+							SpeedChimeActive = false
+							TriggerEvent("initiald:Sound:StopOnOne")
+						end
+					end
+				end
+
+				local curTachometer=""
+				if lightson == 1 or highbeams == 1 then
+					curTachometer = "night_labels_"..labelType
+				else
+					curTachometer = "labels_"..labelType
+				end
+				-- local curSpeedometer = "nodrift_background"
+
+
+				if not gear then gear = 1 end
+				if gear == 1 then gear = 0 end
+
+				if RPM > 0.90 then
+					DrawSprite(cst.ytdName, "rev_light", cst.centerCoords[1]+cst.RevLight[1],cst.centerCoords[2]+cst.RevLight[2],cst.RevLight[3],cst.RevLight[4], 0.0, 255, 255, 255, curAlpha)
+				end
+				--DrawSprite(cst.ytdName, curSpeedometer, cst.centerCoords[1]+cst.SpeedoBGLoc[1],cst.centerCoords[2]+cst.SpeedoBGLoc[2],cst.SpeedoBGLoc[3],cst.SpeedoBGLoc[4], 0.0, 255, 255, 255, curAlpha)
+				--DrawSprite(cst.ytdName, curTachometer, cst.centerCoords[1]+cst.TachoBGloc[1],cst.centerCoords[2]+cst.TachoBGloc[2],cst.TachoBGloc[3],cst.TachoBGloc[4], 0.0, 255, 255, 255, curAlpha)
+				SetOverriddenTexture("tachometerbg", curTachometer)
+				SetOverriddenTexture("speedometerbg", curSpeedometer)
+				--DrawSprite(cst.ytdName, "gear_"..gear, cst.centerCoords[1]+cst.GearLoc[1],cst.centerCoords[2]+cst.GearLoc[2],cst.GearLoc[3],cst.GearLoc[4], 0.0, 255, 255, 255, curAlpha)
+
+				if cst.useKPH then
+					DrawSprite(cst.ytdName, "kmh", cst.centerCoords[1]+cst.UnitLoc[1],cst.centerCoords[2]+cst.UnitLoc[2],cst.UnitLoc[3],cst.UnitLoc[4], 0.0, 255, 255, 255, curAlpha)
+				else
+					DrawSprite(cst.ytdName, "mph", cst.centerCoords[1]+cst.UnitLoc[1],cst.centerCoords[2]+cst.UnitLoc[2],cst.UnitLoc[3],cst.UnitLoc[4], 0.0, 255, 255, 255, curAlpha)
+				end
+				if pedInVehicleSeat == PlayerPed and vehclass >= 0 and vehclass < 13 or vehclass >= 17 then
+					local aboveGround = GetEntityHeightAboveGround(veh)
+					if angle(veh) >= 10 and angle(veh) <= 18 and aboveGround <= 1.5 then
+						driftSprite = "drift_blue"
+						DrawSprite(cst.ytdName, driftSprite, cst.centerCoords[1]+cst.FuelBGLoc[1],cst.centerCoords[2]+cst.FuelBGLoc[2],cst.FuelBGLoc[3],cst.FuelBGLoc[4], 0.0, 255, 255, 255, curDriftAlpha)
+						BlinkDriftText(false)
+					elseif angle(veh) > 18 and aboveGround <= 1.5 then
+						driftSprite = "drift_yellow"
+						DrawSprite(cst.ytdName, driftSprite, cst.centerCoords[1]+cst.FuelBGLoc[1],cst.centerCoords[2]+cst.FuelBGLoc[2],cst.FuelBGLoc[3],cst.FuelBGLoc[4], 0.0, 255, 255, 255, curDriftAlpha)
+						BlinkDriftText(false)
+					elseif angle(veh) < 10 and aboveGround <= 1.5 then
+						driftSprite = "drift_blue"
+						DrawSprite(cst.ytdName, driftSprite, cst.centerCoords[1]+cst.FuelBGLoc[1],cst.centerCoords[2]+cst.FuelBGLoc[2],cst.FuelBGLoc[3],cst.FuelBGLoc[4], 0.0, 255, 255, 255, curDriftAlpha)
+						BlinkDriftText(true)
+					end
+				else
+					curDriftAlpha = 0
+				end
+
+
+			end
+		else
+			Wait(500)
+		end
+	end
+end)
+
+function switchUnit()
+	useKPH = not useKPH
+end
+
+Citizen.CreateThread(function()
+
+	RegisterNetEvent('initiald:Sound:PlayOnOne')
+	AddEventHandler('initiald:Sound:PlayOnOne', function(soundFile, soundVolume, loop)
+	    SendNUIMessage({
+	        transactionType     = 'playSound',
+	        transactionFile     = soundFile,
+	        transactionVolume   = soundVolume,
+			transactionLoop   = loop
+	    })
+	end)
+
+	RegisterNetEvent('initiald:Sound:StopOnOne')
+	AddEventHandler('initiald:Sound:StopOnOne', function()
+	    SendNUIMessage({
+	        transactionType     = 'stopSound'
+	    })
+	end)
+
+end)
